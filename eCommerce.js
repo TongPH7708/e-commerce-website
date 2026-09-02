423eddd6a3a89146a5198baaeea69bde211a357c
TongPH7708 <phuohong26t@forward.edu.my>
2026-09-02 13:59:34 +0800

    Add files via upload

---
+++ eCommerce.js
@@ -0,0 +1,13 @@
+let searchbar = document.getElementById("searchbar");
+
+let products = document.getElementsByClassName("card")
+
+let cart = [];
+
+let addToCart = document.getElementsByClassName("addToCart");
+
+addToCart.addEventListener("click", function(){
+    cart.push('product')
+});
+
+
